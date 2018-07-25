f01adf9f9173d900d2a8effbbf89ba05e61f4fb4
zjzjyuzf <zj_zj_yuzf@163.com>
2018-07-25 20:39:44 +0800

    create ppg

---
+++ src/main/java/com/yuzf/YuzfPpgRememberApplication.java
@@ -0,0 +1,12 @@
+package com.yuzf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuzfPpgRememberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuzfPpgRememberApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yuzf/YuzfPpgRememberApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuzf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YuzfPpgRememberApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
